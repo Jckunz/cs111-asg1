46bb244a709f656836f33fc7258c5e69ba97786f
Alex Jolicoeur <ajolicoe@ucsc.edu>
2013-04-18 15:00:57 -0700

    output, input redirect, command with/without args, exit ALL WORK

--- myshell.c
+++ myshell.c
@@ -1,4 +1,3 @@
-//myshell.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,7 +5,7 @@
 #include <errno.h>
 
 void execute(char **, int, char **);
-int parse(char **, char **, int *);
+int parse(char **, char *[], char **, int *);
 
 #define DEFAULT_MODE         0
 #define BACKGROUND_MODE      1
@@ -19,25 +18,27 @@ extern char **get_line();
 int main(int argc, char *argv[]) {
 	int mode = DEFAULT_MODE;
 	int commandArgc;
-	char *supp = NULL;
-	char **args; 
-	while(1) {
+	char *command[80], *supp = NULL;
+	char **args;
+
+	while (1) {
 		printf("myShell>"); //display prompt
 		args = get_line();
-		if(strcmp(args[0], "exit") == 0) exit(0);
-		commandArgc = parse(args, &supp, &mode);
-		if(strcmp(args[0], "cd") == 0) chdir(args[1]);
+		if (strcmp(args[0], "exit") == 0) exit(0);
+		commandArgc = parse(args, command, &supp, &mode);
+		if (strcmp(args[0], "cd") == 0) chdir(args[1]);
 		else execute(args, mode, &supp);
 		//printf("command arg count: %d\n", commandArgc);
 	}
 	return 0;
 }
 
-int parse(char **input, char **suppPtr, int *modePtr) {
+int parse(char **input, char *command[], char **suppPtr, int *modePtr) {
 	int i, commandArgc = 0;
 	int terminate = 0;
 	*suppPtr = NULL;
-	for(i = 0; input[i] != NULL && !terminate; i++) {
+	command = input;
+	for (i = 0; input[i] != NULL && !terminate; i++) {
 		commandArgc++;
 		if (strcmp(input[i], "&") == 0) {
 			*modePtr = BACKGROUND_MODE;
@@ -45,36 +46,85 @@ int parse(char **input, char **suppPtr, int *modePtr) {
 		} else if (strcmp(input[i], ">") == 0) {
 			*modePtr = OUTPUT_REDIRECT_MODE;
 			*suppPtr = input[i+1];
+			command[i] = '\0';
 			terminate = 1;
 			//printf("running in Output Redirect Mode\n");
 		} else if (strcmp(input[i], "<") == 0) {
 			*modePtr = INPUT_REDIRECT_MODE;
 			*suppPtr = input[i+1];
+			command[i] = '\0';
 			terminate = 1;
 			//printf("running in Input Redirect Mode\n");
 		} else if (strcmp(input[i], "|") == 0) {
 			*modePtr = PIPELINE_MODE;
 			*suppPtr = input[i+1];
+			command[i] = '\0';
 			terminate = 1;
 			//printf("running in Pipeline Mode\n");
 		}
-		printf("Argument %d: %s\n", i, input[i]);
-		if (*suppPtr != NULL) printf("Supplement: %s\n", *suppPtr);
+		//printf("Argument %d: %s\n", i, input[i]);
+		//if (*suppPtr != NULL) printf("Supplement: %s\n", *suppPtr);
 	}
 	return commandArgc;
 }
 
 void execute(char **command, int mode, char **suppPtr) {
-	pid_t pid, pid2;
+	pid_t pid1, pid2;
+	FILE *file;
 	char *supp2 = NULL;
+	char *command2[80];
 	int commandArgc2;
-	int mode2 = DEFAULT_MODE;
+	int mode2 = DEFAULT_MODE, pid1_status, pid2_status;
 	int pipeline[2];
 	if (mode == PIPELINE_MODE) {
 		if (pipe(pipeline)) {
 			fprintf(stderr,"Pipe failed!\n");
 			exit(-1);
 		}
-		//int commandArgc2 = parse (suppPtr, &supp2, &mode2);
+		parse (suppPtr, command2, &supp2, &mode2);
+	}
+	pid1 = fork();
+	if (pid1 < 0) {
+		printf("Error: pid1 < 0\n");
+		exit(1);
+	}  else if (pid1 == 0) {
+		switch (mode) {
+			case OUTPUT_REDIRECT_MODE:
+				file = freopen(*suppPtr, "w+", stdout);
+				dup2(fileno(file), 1);
+				break;
+			case INPUT_REDIRECT_MODE:
+				file = freopen(*suppPtr, "r", stdin);
+				dup2(fileno(file) ,0);
+				break;
+			case PIPELINE_MODE:
+				close(pipeline[0]);
+				dup2(pipeline[1], fileno(stdout));
+				close(pipeline[1]);
+				break;
+		}
+		execvp(*command, command);
+	} else {
+		if (mode == BACKGROUND_MODE);
+		else if (mode == PIPELINE_MODE) {
+			waitpid(pid1, &pid1_status, 0);
+			pid2 = fork();
+			if (pid2 < 0) {
+				printf("Error: pid2 < 0\n");
+				exit(1);
+			} else if (pid2 == 0) {
+				close(pipeline[1]);
+				dup2(pipeline[0], fileno(stdin));
+				close(pipeline[0]);
+				execvp(*command2, command2);
+			} else {
+				;//wait(NULL);
+				//waitpid(pid1, &pid1_status, 0);
+				//waitpid(pid2, &pid2_status, 0);
+				close(pipeline[0]);
+				close(pipeline[1]);
+			}
+		} else waitpid(pid1, &pid1_status, 0);
+		//wait(NULL);
 	}
 }
